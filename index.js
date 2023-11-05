33db9407b0af5a60d0de0c9fa2b90a20ba7eb0df
Alexander <alex.teel93@gmail.com>
2023-11-05 12:41:47 -0500

    resubmission

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+  cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+  cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+  cats.shift();
+}
+
+function appendCat(name) {
+  let newcats = cats.slice()
+  newcats.push(name);
+  return newcats;
+}
+
+function prependCat(name) {
+  let bcats = cats.slice()
+  bcats.unshift(name);
+  return bcats;
+}
+
+function removeLastCat() {
+  let acats = cats.slice()
+  acats.pop();
+  return acats;
+}
+
+function removeFirstCat() {
+  let differentcats = cats.slice();
+  differentcats.shift(); 
+  return differentcats;
+}
